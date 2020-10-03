b5e3339ed0d5945d67edbe06232e3c9dbc98884a
Zach Smelcer <zacaronithesmelcer@icloud.com>
2020-10-03 15:14:39 -0500

    Working on the js file. As of right now, i have an array of the hours for each work hour. The function 'colorCode' takes the currentHour; which is a function using Moment that gets the current time by the hour, and checks the currentTime. If the current time is equal to hoursArray, it shall be red, if the current time is greater than hoursArray its grey, if the current time is less than hoursArray, it shall be green. This seems to prove a working function but it will only count the past hours as grey if by example: you load the page up at 1pm, the row for 1am is red because it is the current hour but all the rest will be green. As time passes, lets say two hours, its now 3pm and the rows for 2pm and 1pm are now green because that's when you loaded the page. I will try to fix this issue as I continue

--- docs/jq.js
+++ docs/jq.js
@@ -26,96 +26,103 @@ var col7 = $("#col7");
 var col8 = $("#col8");
 var col9 = $("#col9");
 
+// var hoursArray = [{"09": 1},{"10": 2},{"11": 3},{"12": 4},{"01": 5},{"02": 6},{"03": 7},{"04": 8},{"05": 9}];
+var hoursArray = ["09","10","11","12","01","02","03","04","05"];
+
+
+
+
 var currentHour = moment().format('hh');
 currentDay.append('' + moment().format('dddd') + ' ' + moment().format('MM/DD/YYYY'));
 
 colorCode();
 
-
+console.log(currentHour);
 
 function colorCode(){
 
-    if(currentHour == "09"){
-        col1.css("background-color", "red");
+    if(currentHour === hoursArray[0]){
+        col1.css("background-color", "red"); 
     }
-    if(currentHour == "10"){
+    if(currentHour === hoursArray[1]){
+        console.log(currentHour);
         col2.css("background-color", "red");
     }
-    if(currentHour == "11"){
+    if(currentHour === hoursArray[2]){
         col3.css("background-color", "red");
     }
-    if(currentHour == "12"){
+    if(currentHour === hoursArray[3]){
         col4.css("background-color", "red");
     }
-    if(currentHour == "01"){
+    if(currentHour === hoursArray[4]){
         col5.css("background-color", "red");
     }
-    if(currentHour == "02"){
+    if(currentHour === hoursArray[5]){
         col6.css("background-color", "red");
     }
-    if(currentHour == "03"){
+    if(currentHour === hoursArray[6]){
         col7.css("background-color", "red");
     }
-    if(currentHour == "04"){
+    if(currentHour === hoursArray[7]){
         col8.css("background-color", "red");
     }
-    if(currentHour == "05"){
+    if(currentHour === hoursArray[8]){
         col9.css("background-color", "red");
     }
 
-    if(currentHour > "09"){
+    if(currentHour > hoursArray[0]){
         col1.css("background-color", "lightgrey");
     }
-    if(currentHour > "10"){
+    if(currentHour > hoursArray[1]){
         col2.css("background-color", "lightgrey");
     }
-    if(currentHour > "11"){
+    if(currentHour > hoursArray[2]){
         col3.css("background-color", "lightgrey");
     }
-    if(currentHour > "12"){
+    if(currentHour > hoursArray[3]){
         col4.css("background-color", "lightgrey");
     }
-    if(currentHour > "01"){
+    if(currentHour > hoursArray[4]){
         col5.css("background-color", "lightgrey");
     }
-    if(currentHour > "02"){
+    if(currentHour > hoursArray[5]){
         col6.css("background-color", "lightgrey");
     }
-    if(currentHour > "03"){
+    if(currentHour > hoursArray[6]){
         col7.css("background-color", "lightgrey");
     }
-    if(currentHour > "04"){
+    if(currentHour > hoursArray[7]){
         col8.css("background-color", "lightgrey");
     }
-    if(currentHour > "05"){
+    if(currentHour > hoursArray[8]){
         col9.css("background-color", "lightgrey");
     }
 
-    if(currentHour < "09"){
+    if(currentHour < hoursArray[0]){
         col1.css("background-color", "green");
     }
-    if(currentHour < "10"){
+    if(currentHour < hoursArray[1]){
         col2.css("background-color", "green");
     }
-    if(currentHour < "11"){
+    if(currentHour < hoursArray[2]){
         col3.css("background-color", "green");
     }
-    if(currentHour < "12"){
+    if(currentHour < hoursArray[3]){
         col4.css("background-color", "green");
     }
-    if(currentHour < "01"){
+    if(currentHour < hoursArray[4]){
         col5.css("background-color", "green");
     }
-    if(currentHour < "02"){
+    if(currentHour < hoursArray[5]){
         col6.css("background-color", "green");
     }
-    if(currentHour < "03"){
+    if(currentHour < hoursArray[6]){
         col7.css("background-color", "green");
     }
-    if(currentHour < "04"){
+    if(currentHour < hoursArray[7]){
         col8.css("background-color", "green");
     }
-    if(currentHour < "05"){
+    if(currentHour < hoursArray[8]){
         col9.css("background-color", "green");
     }
 
